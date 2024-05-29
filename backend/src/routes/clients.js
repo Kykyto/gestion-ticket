3c1cd1440d7bdf83a7a5060b2a9e520a0ec9b2f2
jules <jules.pichot@fluigent.com>
2024-05-29 22:36:27 +0200

    Removed AuthToken

--- backend/src/routes/clients.js
+++ backend/src/routes/clients.js
@@ -4,15 +4,15 @@ const clientController = require('../controllers/clientsController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 // Créer un client
-router.post('/', authMiddleware.verifyToken, clientController.createClient);
+router.post('/', /*authMiddleware.verifyToken*/ clientController.createClient);
 
 // Modifier un client
-router.put('/:id', authMiddleware.verifyToken, clientController.updateClient);
+router.put('/:id', /*authMiddleware.verifyToken*/ clientController.updateClient);
 
 // Supprimer un client
-router.delete('/:id', authMiddleware.verifyToken, clientController.deleteClient);
+router.delete('/:id', /*authMiddleware.verifyToken*/ clientController.deleteClient);
 
 // Obtenir tous les clients
-router.get('/', authMiddleware.verifyToken, clientController.getAllClients);
+router.get('/', /*authMiddleware.verifyToken*/ clientController.getAllClients);
 
 module.exports = router;
